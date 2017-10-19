e6b3238fed49b11e95191a427cf871069e8f37b7
Kruithne <kruithne@gmail.com>
2017-10-19 15:07:23 +0100

    Initial commit. :boom:

---
+++ jenkins96.js
@@ -0,0 +1,11 @@
+/*
+ * Jenkins96 Hashing Algorithm
+ * Originally created by Robert John Jenkins Junior
+ * Implemented in JavaScript for Node by Kruithne <kruithne@gmail.com>
+ *
+ * https://en.wikipedia.org/wiki/Jenkins_hash_function
+ */
+
+module.exports = function() {
+
+};
